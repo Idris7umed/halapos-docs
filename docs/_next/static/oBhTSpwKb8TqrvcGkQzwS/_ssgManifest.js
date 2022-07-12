8d84ce683f13af2ecaf165e92b2d4663afad5f42
Mian Saleem <saleem@tecdiary.com>
2022-07-12 21:09:17 +0800

    Update config

--- docs/_next/static/oBhTSpwKb8TqrvcGkQzwS/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fdocs\u002Fpos-screen","\u002F","\u002Fdocs\u002Fdashboard","\u002Fdocs\u002Fmanage-sales","\u002Fdocs\u002Finstallation"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
